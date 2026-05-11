ead4424f2f163b3573e6004782d74a260b49e6c7
Saumya <saumyakumari1512@gmail.com>
2026-05-11 22:29:45 +0530

    Added logout functionality and premium logout UI

--- resume_builder/resume_builder/urls.py
+++ resume_builder/resume_builder/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),   
     path('', include('builder.urls')),
 ]
